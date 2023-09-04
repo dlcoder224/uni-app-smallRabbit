8da1de4d2d5fec9f338e12e65c4c662d55e768ad
wenlongliu <liuwenlong@111.com>
2023-09-04 16:52:21 +0800

    tabbar，轮播图以及图片预览

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,57 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
-	</view>
+	<swiper class="banner" indicator-dots circular autoplay>
+		<swiper-item v-for="item in pictures" :key="item.id">
+			<image :src="item.url" @tap="onPreviewImage(item.url)"></image>
+		</swiper-item>
+	</swiper>
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				pictures: [
+					{
+						id: '1',
+						url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_1.jpg'
+					},{
+						id: '2',
+						url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_2.jpg'
+					},{
+						id: '3',
+						url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_3.jpg'
+					},{
+						id: '4',
+						url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_4.jpg'
+					},{
+						id: '5',
+						url: 'https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_5.jpg'
+					},
+				]
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+		  onPreviewImage(url) {
+			  uni.previewImage({
+				  urls: this.pictures.map(v => v.url),
+				  current: url
+			  })
+		  } 
 		}
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+<style lang="scss" scoped>
+ .banner {
+	 width: 750rpx;
+	 height: 750rpx;
+	 image {
+		 width: 100%;
+		 height: 100%;
+	 }
+ }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -3,17 +3,52 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      title: "Hello"
+      title: "Hello",
+      pictures: [
+        {
+          id: "1",
+          url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_1.jpg"
+        },
+        {
+          id: "2",
+          url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_2.jpg"
+        },
+        {
+          id: "3",
+          url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_3.jpg"
+        },
+        {
+          id: "4",
+          url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_4.jpg"
+        },
+        {
+          id: "5",
+          url: "https://pcapi-xiaotuxian-front-devtest.itheima.net/miniapp/uploads/goods_preview_5.jpg"
+        }
+      ]
     };
   },
   onLoad() {
   },
-  methods: {}
+  methods: {
+    onPreviewImage(url) {
+      common_vendor.index.previewImage({
+        urls: this.pictures.map((v) => v.url),
+        current: url
+      });
+    }
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: common_vendor.t($data.title)
+    a: common_vendor.f($data.pictures, (item, k0, i0) => {
+      return {
+        a: item.url,
+        b: common_vendor.o(($event) => $options.onPreviewImage(item.url), item.id),
+        c: item.id
+      };
+    })
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "D:/Code/uni-app-smallRabbit/pages/index/index.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-1cf27b2a"], ["__file", "D:/Code/uni-app-smallRabbit/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
